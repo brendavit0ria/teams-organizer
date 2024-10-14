57d97e0af3ff91a09afbce7e8ad0988d48f1f50a
brendavit0ria <brendavitoria0602@gmail.com>
2024-10-14 10:09:58 -0300

    feat: defining the application theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
-import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/theme/theme";
+
+import { Groups } from "@screens/Groups/Groups";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
