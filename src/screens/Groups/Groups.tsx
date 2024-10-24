524dbc4dece7e4a63fcc12bd0b333ebdb1c6091d
brendavit0ria <brendavitoriasts@gmail.com>
2024-10-24 10:46:28 -0300

    feat: creating highlight component

--- src/screens/Groups/Groups.tsx
+++ src/screens/Groups/Groups.tsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles";
 
 import { Header } from "@components/Header/Header";
+import { Highlight } from "@components/Highlight/Highlight";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }
