5b40c0da5b52e3d026c931e382fa0ecbee10a404
brendavit0ria <brendavitoria0602@gmail.com>
2024-10-24 10:00:01 -0300

    feat: icon library

--- App.tsx
+++ App.tsx
@@ -6,12 +6,10 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Loading } from "@components/Loading/Loading";
-import { Header } from "@components/Header/Header";
-
 import theme from "./src/theme/theme";
 
 import { Groups } from "@screens/Groups/Groups";
+import { Loading } from "@components/Loading/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
