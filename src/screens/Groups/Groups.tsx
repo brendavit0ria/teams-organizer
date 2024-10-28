73fc7a52b00b7432e4563c3fada70a6054e68539
brendavit0ria <brendavitoriasts@gmail.com>
2024-10-28 14:07:21 -0300

    feat: listing groups

--- src/screens/Groups/Groups.tsx
+++ src/screens/Groups/Groups.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Container } from "./styles";
 
 import { Header } from "@components/Header/Header";
@@ -5,11 +8,18 @@ import { Highlight } from "@components/Highlight/Highlight";
 import { GroupCard } from "@components/GroupCard/GroupCard";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocketseat"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
