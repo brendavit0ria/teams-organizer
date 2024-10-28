0888a306ab4536a7dc9be749f2c4a03c3861bd0e
brendavit0ria <brendavitoria0602@gmail.com>
2024-10-28 09:11:57 -0300

    feat: creating GroupCard component

--- src/screens/Groups/Groups.tsx
+++ src/screens/Groups/Groups.tsx
@@ -2,12 +2,14 @@ import { Container } from "./styles";
 
 import { Header } from "@components/Header/Header";
 import { Highlight } from "@components/Highlight/Highlight";
+import { GroupCard } from "@components/GroupCard/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <GroupCard title="" />
     </Container>
   );
 }
