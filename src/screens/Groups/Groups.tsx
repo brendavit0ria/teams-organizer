d43cc461441c57acdc5afd0558e8c97bac2a05c0
brendavit0ria <brendavitoria0602@gmail.com>
2024-11-26 15:25:31 -0300

    feat: identifying the focus on the screen

--- src/screens/Groups/Groups.tsx
+++ src/screens/Groups/Groups.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { FlatList } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
 import { Container } from "./styles";
+
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 import { Header } from "@components/Header/Header";
 import { Highlight } from "@components/Highlight/Highlight";
@@ -18,6 +20,21 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate("new");
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
