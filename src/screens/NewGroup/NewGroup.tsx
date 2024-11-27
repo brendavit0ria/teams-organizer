a9f456d0234739421d424eecf24a52076ab59cbe
brendavit0ria <brendavitoriasts@gmail.com>
2024-11-27 10:55:40 -0300

    feat: handling exceptions

--- src/screens/NewGroup/NewGroup.tsx
+++ src/screens/NewGroup/NewGroup.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
+import { Alert } from "react-native";
 
 import { groupCreate } from "@storage/group/groupCreate";
 
@@ -8,6 +9,7 @@ import { Header } from "@components/Header/Header";
 import { Highlight } from "@components/Highlight/Highlight";
 import { Button } from "@components/Button/Button";
 import { Input } from "@components/Input/Input";
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
@@ -16,10 +18,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
     }
   }
 
