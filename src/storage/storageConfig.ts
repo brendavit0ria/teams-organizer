3d67440812fbfa7e19fc955ca601bbff3c225e79
brendavit0ria <brendavitoria0602@gmail.com>
2024-11-28 14:01:08 -0300

    feat: storing players

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUP_COLLECTION = "@teams-organizer:group";
+const PLAYER_COLLECTION = "@teams-organizer:players";
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
